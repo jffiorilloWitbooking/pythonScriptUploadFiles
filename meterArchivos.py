c08ee777fdb83b2d7a22f4f0b337c588e1bd877b
jffiorilloWitbooking <jfiorillo@witbooking.com>
2013-12-05 18:22:21 +0100

    v1.0

--- meterArchivos.py
+++ meterArchivos.py
@@ -1,76 +1,139 @@
+#!/usr/bin/python
+
 import pxssh
 import getpass
 import httplib,urllib,urllib2
+import sys
+import getopt
+import glob,os 
 from lxml import etree, html
 
-def getValues():
-  hostname = raw_input('hostname: ')
-  username = raw_input('username: ')
-  password = getpass.getpass('password: ')
-  return [hostname,username,password]
-
-def getFilesAsString(urlFind,commandFind):
+def getFilesAsString(urlFind,userFtp,passFtp,commandFind):
   try:                                                            
     s = pxssh.pxssh()
-    s.login(urlFind,'root','WitDevel;')
+    s.login(urlFind,userFtp,passFtp)
     s.sendline(commandFind)
     s.prompt()
     content= s.before
     fil = open('entry.txt','w')
     fil.write(content)
     fil.close()
-    #print before 
     s.logout()
     return content
   except pxssh.ExceptionPxssh, e:
       print "pxssh failed on login."
       print str(e)
 
-def sendFile(url,lineR,successLog,errorLog,msg):
-  #print "lineR" , lineR
-  successLog.write(lineR[2])
-  #params = urllib.urlencode({'hotelTicker':lineR[0],'entiy':lineR[1],'idEntity':line[2],'fileNane':lineR[3]})
+def sendFile(url,line,lineR,successLog,errorLog,msg,wait):
   try:
     hotelTicker = lineR[0]
     entity = lineR[1]
     idEntity = int(lineR[2])
     fileName = lineR[3]
-    if hotelTicker == 'hoteldemo.com.v6':
-      print lineR
-      params = urllib.urlencode({'hotelTicker':hotelTicker,'entity':entity,'idEntity':idEntity,'fileName':fileName})
-      print params
-      conn= urllib.urlopen(url+":8080/WitBookerAPI-war/webresources/internal/withotel/insert", params) 
-      htmlGet = html.fromstring(conn.read())
-      print(etree.tostring(htmlGet,pretty_print=True))
-      conn.close()
+    #if hotelTicker == 'hoteldemo.com.v6':
+    print lineR
+    params = urllib.urlencode({'hotelTicker':hotelTicker,'entity':entity,'idEntity':idEntity,'fileName':fileName})
+    print params
+    conn= urllib.urlopen(url+":8080/WitBookerAPI-war/webresources/internal/withotel/insert", params) 
+    htmlGet = html.fromstring(conn.read())
+    msgBonito=etree.tostring(htmlGet,pretty_print=True)
+    successLog.write(str(lineR))
+    successLog.write(str(params))
+    successLog.write(msgBonito)
+    print(msgBonito)
+    conn.close()
   except ValueError:
     print msg
+    if (wait):
+      raw_input('Error happens, enter any key to continue...')
     errorLog.write(msg)
 
+def error():
+  print 'Arguments required. \nPlease run python meterArchivos.py -h for more information.'
+  #print 'test.py -i <inputfile> -o <outputfile>'
+  sys.exit(2)
 
-def main():
-  #url = "http://localhost"
-  url = "http://www3.witbooking.com"
-  urlFind = "www3.witbooking.com"
-  commandFind = 'find /var/www/vhosts/www3.witbooking.com/httpdocs/v6/multimedia/ -name \'*.png\' -or -name \'*.jpg\' -or -name \'*.gif\''
-  content = getFilesAsString(urlFind,commandFind)
+def main(argv):
+  serverHash = {"luke": [
+                  "http://luke.local",
+                  "luke.local",
+                  "/var/www/vhosts/jpala.luke.local/httpdocs/v6/multimedia",
+                  "oper",
+                  "wIT_oper13"
+                ] ,
+            "test" : [
+                  "http://www3.witbooking.com",
+                  "www3.witbooking.com",
+                  "/var/www/vhosts/www3.witbooking.com/httpdocs/v6/multimedia/",
+                  "root",
+                  "WitDevel;"
+                ],
+            "prod" : [
+                  "http://www.witbooking.com",
+                  "www.witbooking.com",
+                  "/var/www/vhosts/www.witbooking.com/httpdocs/v6/multimedia",
+                  "ftpwitbooking",
+                  "freeRock!708"
+                ],
+            #, "local" : "localhost"
+            }
+  server = []
+  wait = False
+  try:
+    opts, args = getopt.getopt(argv,"hs:rw",["server="])
+    #opts, args = getopt.getopt(argv,"hs:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+    error()
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'meterArchivos.py -s <server:luke,test,prod> [-w wait in any directory error] [-r remove old logs]'
+      sys.exit()
+    elif opt in ["-s","--server"]:
+      if serverHash.has_key(arg):
+        server = serverHash[arg]
+      else:
+        print "server: ",arg," not found."
+        print "servers accepted: " , reduce(lambda x,y: y+" , "+x,serverHash.keys(),"")[:-2]
+        sys.exit(2)
+    elif opt == "-r":
+        print "Borrando archivos de logs de corridas anteriories"
+        for fil in glob.glob('logs/*'):
+          os.remove(fil)
+    elif opt == '-w':
+      wait = True
+  if len(argv) == 0 or len(server) == 0:
+    error()
+  url = server[0]
+  urlFind = server[1]
+  commandFind = 'find '+server[2]+' -name \'*.png\' -or -name \'*.jpg\' -or -name \'*.gif\''
+  userFtp = server[3]
+  passFtp = server[4]
+  print "ssh "+userFtp+"@"+urlFind
+  content = getFilesAsString(urlFind,userFtp,passFtp,commandFind)
   errorLog = open('logs/error.log','w')
   successLog = open('logs/success.log','w')
   i = 0
   totalString = content.splitlines(True)[1:]
+  hashMap = {}
   for line in totalString:
     lineRQ =map(lambda x: x.strip(),line.split("/"))
     msg = '\n'+str(line.strip())+" len: "+str(len(lineRQ))
     if len(lineRQ) >= 8:
       lineR = lineRQ[-4:]
+      hotelTicker = lineR[0]
+      if hashMap.has_key(hotelTicker):
+        hashMap[hotelTicker] = hashMap[hotelTicker] + 1
+      else:
+        hashMap[hotelTicker] = 1
       i+=1
-      #print 'llamar con ',lineR
-      sendFile(url,lineR,successLog,errorLog,msg)
+      sendFile(url,line,lineR,successLog,errorLog,msg,wait)
     else:
       print msg
       errorLog.write(msg)
   print "i : " , i, "total: " , len(totalString)
+  print hashMap
   errorLog.close()
   successLog.close()
 
-main()
+if __name__ == "__main__":
+  main(sys.argv[1:])
